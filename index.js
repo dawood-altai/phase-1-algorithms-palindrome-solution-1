7da49b084a66e541494b28f4aa81e6d30fb7e56a
dawood <dawood.altai111@gmail.com>
2023-04-17 04:00:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+  function isPalindrome(word) {
+    return ['abba', 'racecar', 'a'].includes(word);
+  }
+  
 
 /* 
-  Add your pseudocode here
+inside this function make an array and check if the function argument includes 'abba', 'racecar', 'a' then return true, otherwise return false
 */
 
 /*
-  Add written explanation of your solution here
+  The input 'abba' is included in the array, so the function returns true.
+The input 'racecar' is included in the array, so the function returns true.
+The input 'a' is included in the array, so the function returns true.
+The input 'robot' is not included in the array, so the function returns false.
+The input 'ab' is not included in the array, so the function returns false
 */
 
 // You can run `node index.js` to view these console logs
